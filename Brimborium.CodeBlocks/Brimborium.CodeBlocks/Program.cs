93d3500ff7af3cc2638edf02a5896d7dbed9f7e9
FlorianGrimm <grimmuko@googlemail.com>
2021-08-28 19:17:05 +0200

    Brimborium.CodeBlocks

---
+++ Brimborium.CodeBlocks/Brimborium.CodeBlocks/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Brimborium.CodeBlocks
+{
+    class Program
+    {
+        static int Main(string[] args) {
+            if (args.Length == 1 && args[0]=="install") {
+                Console.WriteLine($"Brimborium.CodeBlocks {ThisAssembly.AssemblyInformationalVersion}");
+                Console.WriteLine($"tool install");
+
+                var p =System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(
+                    "dotnet.exe"
+                    ) {
+                    ArgumentList = { 
+                        "tool",
+                        "install",
+                        "--tool-path",
+                        "dotnetcodeblocks",
+                        "--add-source",
+                        "./nupkg",
+                        "--version",
+                        ThisAssembly.AssemblyInformationalVersion, //"1.0.5-beta-ga769bb6dcc",
+                        "dotnetcodeblocks"
+                    }
+                });
+                p.WaitForExit();
+                return 0;
+            }
+            if (args.Length == 1 && args[0] == "uninstall") {
+                Console.WriteLine($"Brimborium.CodeBlocks {ThisAssembly.AssemblyInformationalVersion}");
+                Console.WriteLine($"tool install");
+
+                var p = System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(
+                    "dotnet.exe"
+                    ) {
+                    ArgumentList = {
+                        "tool",
+                        "uninstall",
+                        "--tool-path",
+                        "dotnetcodeblocks",
+                        "dotnetcodeblocks"
+                    }
+                });
+                p.WaitForExit();
+                return 0;
+            }
+            Console.WriteLine($"Brimborium.CodeBlocks {ThisAssembly.AssemblyInformationalVersion}");
+            
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
---
+++ Brimborium.CodeBlocks/README.md
@@ -0,0 +1,21 @@
+# Brimborium.CodeBlocks
+
+dotnet core 5
+
+work in progress
+
+cd Brimborium.Net\Brimborium.CodeBlocks
+
+dotnet build
+
+dotnet tool install --tool-path dotnetcodeblocks --add-source ./nupkg --version 1.0.5-beta-ga769bb6dcc dotnetcodeblocks -v d
+
+dotnet tool install --tool-path dotnetcodeblocks --add-source ./nupkg dotnetcodeblocks -v d
+
+dotnet tool uninstall --tool-path dotnetcodeblocks dotnetcodeblocks
+
+
+dotnet run --project Brimborium.CodeBlocks -- install
+
+
+dotnet codeblocks
