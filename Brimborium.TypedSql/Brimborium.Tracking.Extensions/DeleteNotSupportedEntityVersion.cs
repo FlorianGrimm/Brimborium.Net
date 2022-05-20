8ace454d853e94fb1750d0d85b21d111513132b2
Florian Grimm <grimmuko@gmail.com>
2022-05-20 07:40:23 +0200

    add DeleteNotSupportedEntityVersion

---
+++ Brimborium.TypedSql/Brimborium.Tracking.Extensions/DeleteNotSupportedEntityVersion.cs
@@ -0,0 +1,19 @@
+﻿namespace Brimborium.Tracking.Extensions;
+
+public sealed class DeleteNotSupportedEntityVersion<TValue> : ITrackingSetEvent<TValue>
+    where TValue : class, IEntityWithVersion {
+    private static DeleteNotSupportedEntityVersion<TValue>? _Instance;
+    public static DeleteNotSupportedEntityVersion<TValue> Instance => _Instance ??= new DeleteNotSupportedEntityVersion<TValue>();
+
+    private DeleteNotSupportedEntityVersion() { }
+
+    public void OnAdding(TValue value, TrackingSet<TValue> trackingSet, TrackingContext trackingContext) {
+    }
+
+    public void OnUpdating(TValue newValue, TValue oldValue, TrackingStatus oldTrackingStatus, TrackingSet<TValue> trackingSet, TrackingContext trackingContext) {
+    }
+
+    public void OnDeleting(TValue newValue, TValue oldValue, TrackingStatus oldTrackingStatus, TrackingSet<TValue> trackingSet, TrackingContext trackingContext) {
+        throw new InvalidModificationException("Deleteing is not supported");
+    }
+}
