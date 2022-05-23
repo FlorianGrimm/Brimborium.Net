62e2b083785c3a1fade9596626b323920c9fa1df
Florian Grimm <grimmuko@gmail.com>
2022-05-23 21:56:32 +0200

    fix HomeFolder

--- Brimborium.Extensions.Logging.LocalFile/src/SiteConfigurationProvider.cs
+++ Brimborium.Extensions.Logging.LocalFile/src/SiteConfigurationProvider.cs
@@ -10,10 +10,14 @@ public class SiteConfigurationProvider
 {
     public static IConfiguration GetAzureLoggingConfiguration(IWebAppContext context, string configurationFile)
     {
-        var settingsFolder = Path.Combine(context.HomeFolder, "site", "diagnostics");
-        var settingsFile = Path.Combine(settingsFolder, "settings.json");
         var configurationBuilder=new ConfigurationBuilder()
             .AddEnvironmentVariables();
+        if (!string.IsNullOrEmpty(context.HomeFolder)){
+            var settingsFolder = Path.Combine(context.HomeFolder, "site", "diagnostics");
+            var settingsFile = Path.Combine(settingsFolder, "settings.json");
+            configurationBuilder
+                .AddJsonFile(settingsFile, optional: true, reloadOnChange: true);
+        }
         if (!string.IsNullOrEmpty(configurationFile)){
             configurationBuilder
                 .AddJsonFile(configurationFile, optional: true, reloadOnChange: true);
