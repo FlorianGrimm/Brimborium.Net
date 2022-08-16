8991101a27cf9eb36faa8dded04cb9418558a522
Florian Grimm <grimmuko@gmail.com>
2022-08-16 04:41:31 +0200

    fixing DeletingArgument

--- Brimborium.TypedSql/Brimborium.Tracking/ValidateBaseTrackingSet.cs
+++ Brimborium.TypedSql/Brimborium.Tracking/ValidateBaseTrackingSet.cs
@@ -15,22 +15,35 @@ public class ValidateBaseTrackingSet<TKey, TValue> : ITrackingSetEvent<TKey, TVa
     //    this.Validate(newValue);
     //}
 
+    /// <summary>
+    /// Called by Adding, Updating, Deleting
+    /// </summary>
+    protected virtual void ValidateCore(TValue value, ITrackingContext trackingContext, ITrackingSet<TKey, TValue> trackingSet) {
+        // no need to call this
+    }
+
+    /// <summary>
+    /// Called by Adding, Updating
+    /// </summary>
     protected virtual void Validate(TValue value, ITrackingContext trackingContext, ITrackingSet<TKey, TValue> trackingSet) {
         // no need to call this
     }
 
+
     public virtual AddingArgument<TKey, TValue> OnAdding(AddingArgument<TKey, TValue> argument) {
+        this.ValidateCore(argument.Value, argument.TrackingContext, argument.TrackingSet);
         this.Validate(argument.Value, argument.TrackingContext, argument.TrackingSet);
         return argument;
     }
 
     public virtual UpdatingArgument<TKey, TValue> OnUpdating(UpdatingArgument<TKey, TValue> argument) {
+        this.ValidateCore(argument.Value, argument.TrackingContext, argument.TrackingSet);
         this.Validate(argument.NewValue, argument.TrackingContext, argument.TrackingSet);
         return argument;
     }
 
     public virtual DeletingArgument<TKey, TValue> OnDeleting(DeletingArgument<TKey, TValue> argument) {
-        this.Validate(argument.NewValue, argument.TrackingContext, argument.TrackingSet);
+        this.ValidateCore(argument.Value, argument.TrackingContext, argument.TrackingSet);
         return argument;
     }
 }
