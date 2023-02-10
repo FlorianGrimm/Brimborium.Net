1e22d2d476ce9e7f8a67efbe1d4b1ddb04835559
Florian Grimm <schlanger@solvin.com>
2023-02-10 10:31:45 +0100

    step

---
+++ Brimborium.SourceGenerator.CustomizeTemplate/Brimborium.SourceGenerator.CustomizeTemplate/CustomizeTemplateGenerator.cs
@@ -0,0 +1,36 @@
+﻿namespace Brimborium.SourceGenerator.CustomizeTemplate;
+
+[Generator]
+public class CustomizeTemplateGenerator : ISourceGenerator {
+
+    public void Initialize(GeneratorInitializationContext context) {
+        //if (Debugger.IsAttached) {
+        //    // Debugger.Break();
+        //} else { 
+        //    Debugger.Launch();
+        //}
+        context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+    }
+
+    public void Execute(GeneratorExecutionContext context) {
+        if (context.SyntaxReceiver is not SyntaxReceiver syntaxReceiver) { return; }
+        
+        // context.Compilation.GetTypeByMetadataName("Brimborium.BaseMethods.CustomEqualityAttribute");
+        
+    }
+    
+    class SyntaxReceiver : ISyntaxReceiver {
+        readonly List<SyntaxNode> _candidateSyntaxes = new List<SyntaxNode>();
+
+        public IReadOnlyList<SyntaxNode> CandidateSyntaxes => this._candidateSyntaxes;
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode) {
+            if (syntaxNode is ClassDeclarationSyntax 
+                || syntaxNode is RecordDeclarationSyntax 
+                || syntaxNode is StructDeclarationSyntax) {
+                this._candidateSyntaxes.Add(syntaxNode);
+            }
+            syntaxNode.HasStructuredTrivia
+        }
+    }
+}
---
+++ Brimborium.SourceGenerator.CustomizeTemplate/Idea/ClassGenerated1.cs
@@ -0,0 +1,25 @@
+﻿namespace Idea {
+    // [Template]
+    public class ClassTemplate1 {
+        public ClassTemplate1() {
+        }
+
+        // <Replacement>
+        public int A { get; set; }
+        // </Replacement>
+    }
+
+    // [Transformation]
+    public class ClassTransform1 {
+    }
+
+    // [Generated]
+    public class ClassGenerated1 {
+        public ClassGenerated1() {
+        }
+        
+        // <Replacement>
+        public int AB { get; set; }
+        // </Replacement>
+    }
+}
